45b967d64a00c62f97a820a6a57790db39d2adfb
ameklou <ameklou@ameklous-MacBook-Pro.local>
2019-11-09 06:59:40 +0000

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -36,6 +36,14 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
+
+
+    public void createPhoneIntent(View view){
+        Uri uri=Uri.parse("tel:0123456789");
+        Intent phoneIntent= new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(phoneIntent);
+
+    }
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
